fcc05b43b96cc9606498f9ac3419ae25f97c1a32
Mr Muhammadqodir <143997480+accauntforwork@users.noreply.github.com>
2024-03-02 20:29:40 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <div className="bg-gray-600">
+      <div className="align-element py-3 text-center text-white">
+        <h1>
+          by{" "}
+          <a className="font-bold" href="https://github.com/asadbekmuminov">
+            Baxtiyorov M
+          </a>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
